2240e1e44f7f779bab71c07d9c116c55843fe264
huan.tran <huan.tran@gss-sol.com>
2025-06-23 21:27:24 +0700

    PWA
    - Push to web and android

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
 };
-
-export default nextConfig;
+// Configure PWA options
+const pwaOptions = {
+  dest: 'public', // Thư mục chứa service worker
+  register: true, // Tự động đăng ký service worker
+  skipWaiting: true, // Bỏ qua chờ đợi khi có bản cập nhật service worker
+  // disable: process.env.NODE_ENV === 'development', // Tắt PWA trong môi trường dev để tránh lỗi
+};
+// Apply the PWA plugin with the options
+const withPWAConfig = withPWA(pwaOptions);
+// Export the final configuration with both plugins applied
+export default withPWAConfig(nextConfig);
